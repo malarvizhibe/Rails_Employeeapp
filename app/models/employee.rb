15eb3d90eee2cd73cfd491485838f1b240fb9d63
root <root@localhost.localdomain>
2017-09-10 17:00:37 +0000

    Change

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,6 @@
 class Employee < ApplicationRecord
 validates :name, presence: true
 validates_numericality_of :age, :greater_than => 18
+validates :gender, presence: true
+validates :role, presence: true
 end
